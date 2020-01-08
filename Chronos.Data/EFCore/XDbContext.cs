ac2d404a6e7649586ee90bfa5ef8cc645e19c675
Asan Sultan <asan.sultan@mail.com>
2020-01-08 22:59:47 +0600

    [data] Added DbContext, MsSql, Repository

---
+++ Chronos.Data/EFCore/XDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Chronos.Data.EFCore
+{
+    public abstract class XDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        protected XDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected string ConnectionString => Configuration.GetConnectionString(DbContextName);
+
+        protected abstract string DbContextName { get; }
+
+    }
+}
---
+++ Chronos.Data/EFCore/XMsSqlDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Chronos.Data.EFCore
+{
+    public abstract class ScMsSqlDbContext : XDbContext
+    {
+        protected ScMsSqlDbContext(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+    }
+}
