142e5b34fadc62a908dc21767f48f244aa431f53
Asan Sultan <asan.sultan@mail.com>
2020-01-09 00:22:39 +0600

    [web] Added custom blazor components :: Inputs, Redirect

---
+++ Chronos.Web/Components/Forms/IXInputParams.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Chronos.Web.Components.Forms
+{
+    public interface IXInputParams
+    {
+        string Id { get; set; }
+        string Label { get; set; }
+        bool? FormControl { get; set; }
+        bool? ShowValidationMessage { get; set; }
+        string DivClass { get; set; }
+        string LabelClass { get; set; }
+        string CssClass { get; set; }
+        string ValidationClass { get; set; }
+        Expression<Func<string>> ValidationFor { get; set; }
+    }
+}
---
+++ Chronos.Web/Components/Forms/XInputBaseString.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Chronos.Web.Components.Forms
+{
+    public class XInputBaseString : InputBase<string>, IXInputParams
+    {
+        [Parameter] public string Id { get; set; }
+        [Parameter] public string Label { get; set; }
+        [Parameter] public bool? FormControl { get; set; }
+        [Parameter] public bool? ShowValidationMessage { get; set; }
+        [Parameter] public string DivClass { get; set; }
+        [Parameter] public string LabelClass { get; set; }
+        [Parameter] public new string CssClass { get; set; }
+        [Parameter] public string ValidationClass { get; set; }
+        [Parameter] public Expression<Func<string>> ValidationFor { get; set; }
+
+        [CascadingParameter] public IXInputParams CascadingParams { get; set; }
+
+        protected bool SummaryFormControl => FormControl ?? CascadingParams?.FormControl ?? false;
+        protected bool SummaryShowValidationMessage => ShowValidationMessage ?? CascadingParams.ShowValidationMessage ?? false;
+
+        protected string SummaryDivClass =>
+            $"{(SummaryFormControl ? "form-control-wrapper" : string.Empty)} {DivClass ?? CascadingParams?.DivClass}";
+
+        protected string SummaryLabelClass =>
+            $"{(SummaryFormControl ? "form-control-label" : string.Empty)} {LabelClass ?? CascadingParams?.LabelClass}";
+
+        protected string SummaryCssClass =>
+            $"{(SummaryFormControl ? "form-control" : string.Empty)} " +
+            $"{CssClass ?? (string.IsNullOrWhiteSpace(base.CssClass) ? null : base.CssClass) ?? CascadingParams?.CssClass}";
+
+        protected string SummaryValidationClass =>
+            $"{(SummaryFormControl ? "form-control-validation" : string.Empty)} {ValidationClass ?? CascadingParams?.ValidationClass}";
+
+        protected override bool TryParseValueFromString(string value, out string result,
+            out string validationErrorMessage)
+        {
+            result = value;
+            validationErrorMessage = null;
+            return true;
+        }
+
+    }
+}
